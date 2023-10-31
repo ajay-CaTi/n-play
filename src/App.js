f06a4904a3b087d1036988ee8c0da06edf5c121d
ajay-CaTi <ajaykumarsingh110@gmail.com>
2023-10-31 12:20:45 +0530

    configured tailwindcss, redux, someplanning

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+// import { Provider } from "react-redux";
+// import store from "./component/utils/store";
+
+import Header from "./component/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <Provider store={store}>
+    // </Provider>
+    <div>
+      <h1 className="text-3xl font-bold underline bg-slate-400">Hello</h1>
+      <Header />
     </div>
   );
 }
